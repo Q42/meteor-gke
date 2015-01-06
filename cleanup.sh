5f0d71b43d116e89133a6473a5e0d18e068cc0fb
Christiaan Hees <mail@chees.info>
2015-01-06 17:57:11 +0100

    Don't ask for confirmation when deleting

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 gcloud preview container clusters delete meteor
-gcloud compute forwarding-rules delete meteor
-gcloud compute target-pools delete meteor
-gcloud compute firewall-rules delete meteor-80
+gcloud compute forwarding-rules delete meteor --quiet
+gcloud compute target-pools delete meteor --quiet
+gcloud compute firewall-rules delete meteor-80 --quiet
