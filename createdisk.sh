d5f21da03498fe26eda09aa00109ecfbf964c420
Christiaan Hees <mail@chees.info>
2015-01-06 17:57:21 +0100

    Cleanup

--- createdisk.sh
+++ createdisk.sh
@@ -6,6 +6,5 @@ ZONE=europe-west1-c
 
 gcloud compute disks create --size=$DISK_SIZE --zone=$ZONE $DISK_NAME
 gcloud compute instances attach-disk --zone=$ZONE --disk=$DISK_NAME --device-name temp-data k8s-meteor-master
-gcloud compute ssh --zone=$ZONE k8s-meteor-master \
-  --command "sudo mkdir /mnt/tmp && sudo /usr/share/google/safe_format_and_mount /dev/disk/by-id/google-temp-data /mnt/tmp"
+gcloud compute ssh --zone=$ZONE k8s-meteor-master --command "sudo mkdir /mnt/tmp && sudo /usr/share/google/safe_format_and_mount /dev/disk/by-id/google-temp-data /mnt/tmp"
 gcloud compute instances detach-disk --zone=$ZONE --disk $DISK_NAME k8s-meteor-master
