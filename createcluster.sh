845b77566c7e3f3832f82738738ba1f24fd12975
Christiaan Hees <mail@chees.info>
2015-01-06 17:54:15 +0100

    Wait for Mongo to start up before creating node pods

--- createcluster.sh
+++ createcluster.sh
@@ -10,6 +10,16 @@ gcloud preview container clusters create meteor
 gcloud preview container pods create --config-file mongo-pod.json
 gcloud preview container services create --config-file mongo-service.json
 
+echo "Waiting for Mongo to start..."
+while true; do
+  MONGO_STATUS=`gcloud preview container pods describe mongo | tr -d '\n' | tr ' ' '\n' | tail -n1`
+  if [ "$MONGO_STATUS" == "Running" ]; then
+    break
+  fi
+  printf "."
+  sleep 2
+done
+
 gcloud preview container replicationcontrollers create --config-file meteor-controller.json
 gcloud preview container services create --config-file meteor-service.json
 
