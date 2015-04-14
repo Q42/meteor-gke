d21ce5a28fc7f20219e4ad197eb85d77ff4df26c
Christiaan Hees <mail@chees.info>
2015-04-14 11:11:01 +0200

    Switched commands from gcloud preview container to gcloud alpha container

--- cleanup.sh
+++ cleanup.sh
@@ -3,7 +3,7 @@
 echo "Deleting the cluster, load balancer and firewall rules."
 echo "This won't delete the persistent disk for Mongo, so setting up the cluster again will get you back in the same state."
 
-gcloud preview container clusters delete meteor
+gcloud alpha container clusters delete meteor
 gcloud compute forwarding-rules delete meteor --quiet
 gcloud compute target-pools delete meteor --quiet
 gcloud compute firewall-rules delete meteor-80 --quiet
--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ ZONE=europe-west1-c
 # TODO set CLOUDSDK_CONTAINER_CLUSTER ?
 
 # Create a new cluster called "meteor". This will take a few minutes.
-gcloud preview container clusters create meteor
+gcloud alpha container clusters create meteor
 
 
 if gcloud compute disks list $DISK_NAME | grep READY > /dev/null
@@ -29,12 +29,12 @@ fi
 
 
 # Create pods and a service based on the JSON configuration
-gcloud preview container kubectl create -f mongo-pod.json
-gcloud preview container kubectl create -f mongo-service.json
+gcloud alpha container kubectl create -f mongo-pod.json
+gcloud alpha container kubectl create -f mongo-service.json
 
 echo "Waiting for Mongo to boot before creating the Meteor pods..."
 while true; do
-  MONGO_STATUS=`gcloud preview container kubectl describe pods mongo | grep Status: | awk '{print $2}'`
+  MONGO_STATUS=`gcloud alpha container kubectl describe pods mongo | grep Status: | awk '{print $2}'`
   if [ "$MONGO_STATUS" == "Running" ]; then
     echo
     break
@@ -47,8 +47,8 @@ done
 sleep 20
 
 # Create our replication controller and service, also based on the JSON configuration
-gcloud preview container kubectl create -f meteor-controller.json
-gcloud preview container kubectl create -f meteor-service.json
+gcloud alpha container kubectl create -f meteor-controller.json
+gcloud alpha container kubectl create -f meteor-service.json
 
 # We need to configure the correct firewall rules or nothing will get through
 gcloud compute firewall-rules create meteor-80 --allow=tcp:80 --target-tags k8s-meteor-node
