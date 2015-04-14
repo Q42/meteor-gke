6ec30f657499c147451346910e09fb3433921344
Christiaan Hees <mail@chees.info>
2015-04-14 16:02:55 +0200

    cleanup.sh now uses new names for forwarding rules and target pools

--- cleanup.sh
+++ cleanup.sh
@@ -4,6 +4,7 @@ echo "Deleting the cluster, load balancer and firewall rules."
 echo "This won't delete the persistent disk for Mongo, so setting up the cluster again will get you back in the same state."
 
 gcloud alpha container clusters delete meteor
-gcloud compute forwarding-rules delete meteor --quiet
-gcloud compute target-pools delete meteor --quiet
+#gcloud compute forwarding-rules delete meteor --quiet
+gcloud compute forwarding-rules delete k8s-meteor-default-meteor --quiet
+gcloud compute target-pools delete k8s-meteor-default-meteor --quiet
 gcloud compute firewall-rules delete meteor-80 --quiet
